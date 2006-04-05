5dc9a014afe90d3067a84df837d345cae7f72e99
ultramancool <ultramancool@b23abe28-c204-0410-8fa2-e71ee1c59199>
2006-04-05 22:40:21 +0000

    install.php

---
+++ phpnub/trunk/install.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <input>
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+  <title>PHPNub installer</title>
+
+
+</head>
+
+
+<body>
+
+<div style="text-align: center;">
+<form method="post" action="install2.php" name="Install"><big><big>PHPNub
+Installer<br>
+
+Installer version 1.0<br>
+
+  <small><small><small>Host</small></small></small>
+  <br>
+
+  <input name="dbhost"><br>
+
+  <small><small><small>User<br>
+
+  </small></small></small>&nbsp;<input name="dbuser"><small><small><small><br>
+
+Pass</small> </small></small><br>
+
+  <input name="dbpass"><br>
+
+  <small><small><small>Database Name<br>
+
+  </small></small></small><input name="dbname"><br>
+  <input type="submit"><input type="reset"><br>
+  </big></big></form>
+
+<br>
+
+</div>
+
+</body>
+</html>
