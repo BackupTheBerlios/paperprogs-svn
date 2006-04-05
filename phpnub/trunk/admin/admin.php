b11686dc022f916abaf90e1fc64f76dc223e0c0a
ultramancool <ultramancool@b23abe28-c204-0410-8fa2-e71ee1c59199>
2006-04-05 21:19:24 +0000

    Made it easy(tm) to lock page

--- phpnub/trunk/admin/admin.php
+++ phpnub/trunk/admin/admin.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-  <?php include 'config.php';
-
+<?php
+include 'config.php';
+include 'lock.php';
 include 'includeskin.php';
 ?>
 
