3eb5f9c7d417c8f837eec970b33e0477b1d36954
ultramancool <ultramancool@b23abe28-c204-0410-8fa2-e71ee1c59199>
2006-04-05 20:36:23 +0000

    added login ui

---
+++ phpnub/trunk/login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+<?php include 'config.php';?><?php include 'includeskin.php';?>
+</head>
+
+
+
+	<body>
+
+<!-- For non-visual or non-stylesheet-capable user agents -->
+<div id="mainlink"><a href="#main">Skip to
+main content.</a></div>
+
+<!-- ======== Header ======== -->
+<div id="header">
+<div class="left">
+<p><a href="nub.php">PHP<span class="alt">Nub</span></a></p>
+
+</div>
+
+<div class="right"> <span class="hidden">Useful
+links:</span>&nbsp;
+</div>
+
+<div style="text-align: center;" class="subheader">
+<span class="hidden">Navigation</span>
+Version </div>
+
+</div>
+
+<!-- ======== Main Content ======== -->
+<div id="main">
+<div id="navhead">
+<hr /> <span class="hidden">Path to this page:</span>
+<form method="post" action="login2.php" name="Nub">Username:
+&nbsp;<input size="25" name="user" /><br />
+
+Password: &nbsp;<input size="25" name="Pass" /><br />
+
+  <input type="submit" /><input type="reset" /></form>
+
+Commands should be in format: googlephpnub<br />
+
+</div>
+
+</div>
+
+<p style="border: 1px dashed black; padding: 5px; font-size: 8pt; font-weight: normal;">
+<u><b>The Latest News:</b></u><br />
+
+</p>
+
+<div style="text-align: center;" id="footer">
+<hr style="margin-left: auto; margin-right: auto;" />
+Copyleft Paperprogs &copy; 2004&ndash;06, Template by John
+Zaitseff.<span class="notprinted"></span> <br />
+
+</div>
+
+</body>
+</html>
