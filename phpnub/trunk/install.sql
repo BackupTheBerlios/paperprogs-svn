4c7ffca68ec131a31e81cf5189a4932091495d59
ultramancool <ultramancool@b23abe28-c204-0410-8fa2-e71ee1c59199>
2006-04-05 22:43:52 +0000

    install.php

--- phpnub/trunk/install.sql
+++ phpnub/trunk/install.sql
@@ -45,7 +45,6 @@ CREATE TABLE IF NOT EXISTS `phpnub_users` (
 -- Dumping data for table `phpnub_users`
 -- 
 
-INSERT INTO `phpnub_users` (`id`, `user`, `passhash`) VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(2, 'drapeaua', 'ae1c1768eee0889f8fb8ef858638e221');
+INSERT INTO `phpnub_users` (`id`, `user`, `passhash`) VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3')
 
 
