41b6918004a75888950e39472f29af87a420db61
ultramancool <ultramancool@b23abe28-c204-0410-8fa2-e71ee1c59199>
2006-07-05 17:49:19 +0000

    .

--- PHVault2/trunk/admin/lock.php
+++ PHVault2/trunk/admin/lock.php
@@ -1,17 +1,12 @@
 <?php
 $uname=strip_tags($_COOKIE["uname"]);
-#echo $uname . "<br>";
-#$cleanpage=mysql_real_escape_string($realpage);
 $subpass=strip_tags($_COOKIE["pass"]);
-#echo $subpass . "<br>";
-#$cleanpage=mysql_real_escape_string($realpage);
 mysql_connect($host,$username,$password);
         @mysql_select_db($database) or die( "Unable to select database");
 
         $query2="SELECT * FROM phpnub_users WHERE user='$uname' AND passhash='$subpass'";
         $result=mysql_query($query2);
         $num=mysql_numrows($result);
-#echo $num;
 if (!($num > 0)) { echo "incorrent username or password"; exit;}
 
 ?>
